b471cb33aa99959afa2a1b7132f388d6707e80d5
alinhi75 <sayedali.noohi@studenti.polito.it>
2025-02-21 11:21:35 +0100

    add home page

---
+++ my-website/resources/js/component/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="text-center mt-10">
+        <h1 class="text-4xl font-bold">Welcome to My Website</h1>
+        <p class="text-lg mt-4">I'm a Master's Student in Computer Engineering and a Developer.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home'
+}
+</script>
